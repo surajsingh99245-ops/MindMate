91fb4a8c4094503ada6f6a3d375e228e765b2bfa
Salim Ali <salimali07869@gmail.com>
2026-07-25 19:15:52 +0530

    create separate files for js

---
+++ Frontend/script.js/landing.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 30) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
