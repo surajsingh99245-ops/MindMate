9ff9d4e4ef06094bc1a6e6aab0b3591c000980c2
SURAJ SINGH <250795727+surajsingh99245-ops@users.noreply.github.com.>
2026-07-27 14:09:27 +0530

    Connected to PostgreSQL

--- Backend/server.js
+++ Backend/server.js
@@ -1,51 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
 
 const app = express();
 
+// Test Database Connection
+pool.connect()
+    .then(() => {
+        console.log("✅ Connected to PostgreSQL");
+    })
+    .catch((err) => {
+        console.error("❌ Database connection failed:");
+        console.error(err);
+    });
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
-
-// Home Route
-app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "MindMate Backend Running 🚀"
-    });
-});
-
-// About Route
-app.get("/about", (req, res) => {
-    res.json({
-        project: "MindMate",
-        version: "1.0",
-        developer: "Suraj"
-    });
-});
-
-// Hello Route
-app.get("/hello", (req, res) => {
-    res.json({
-        message: "Hello Suraj!",
-        learning: "Backend is fun 🚀"
-    });
-});
-
-// Login Route
-app.post("/login", (req, res) => {
-    console.log("Request Body:", req.body);
-
-    res.status(200).json({
-        success: true,
-        message: "Login request received!",
-        data: req.body
-    });
-});
-
-// Start Server
-app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
