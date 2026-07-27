56522d31c7f777b606b6c976a634d605f8cdba77
SURAJ SINGH <250795727+surajsingh99245-ops@users.noreply.github.com.>
2026-07-27 11:57:50 +0530

    Handle POST requests and enable CORS

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 const PORT = 3000;
 
+// Home Route
 app.get("/", (req, res) => {
     res.json({
         success: true,
         message: "MindMate Backend Running 🚀"
     });
 });
+
+// About Route
 app.get("/about", (req, res) => {
     res.json({
         project: "MindMate",
@@ -16,6 +25,27 @@ app.get("/about", (req, res) => {
         developer: "Suraj"
     });
 });
+
+// Hello Route
+app.get("/hello", (req, res) => {
+    res.json({
+        message: "Hello Suraj!",
+        learning: "Backend is fun 🚀"
+    });
+});
+
+// Login Route
+app.post("/login", (req, res) => {
+    console.log("Request Body:", req.body);
+
+    res.status(200).json({
+        success: true,
+        message: "Login request received!",
+        data: req.body
+    });
+});
+
+// Start Server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
