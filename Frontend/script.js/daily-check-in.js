4209e10a52a9a9e1a70b191d078c0a8fdfdd9c09
Salim Ali <salimali07869@gmail.com>
2026-07-29 17:53:56 +0530

    now profile dropdown(daily-checkin) working

--- Frontend/script.js/daily-check-in.js
+++ Frontend/script.js/daily-check-in.js
@@ -1,6 +1,5 @@
-const profile = document.querySelector(".profile");
-const profileBtn = document.querySelector(".profile-btn");
 const loggedUser = document.getElementById("loggedUser");
+
 
 if (loggedUser) {
     loggedUser.textContent = localStorage.getItem("username");
@@ -12,4 +11,40 @@ if (logoutBtn) {
     logoutBtn.addEventListener("click", () => {
         localStorage.removeItem("username");
     });
+}
+
+// ================= PROFILE DROPDOWN =================
+
+const checkinProfile =
+    document.querySelector(".profile-checkin");
+
+const checkinProfileBtn =
+    document.querySelector(".profile-btn-checkin");
+
+
+if (checkinProfile && checkinProfileBtn) {
+
+    checkinProfileBtn.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        e.stopPropagation();
+
+        checkinProfile.classList.toggle("active");
+
+        console.log("Profile clicked");
+
+    });
+
+
+    document.addEventListener("click", function (e) {
+
+        if (!checkinProfile.contains(e.target)) {
+
+            checkinProfile.classList.remove("active");
+
+        }
+
+    });
+
 }
