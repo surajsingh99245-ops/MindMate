4e554ac5017c54135f0f219fc0a6511c4a0e3b0a
SURAJ SINGH <250795727+surajsingh99245-ops@users.noreply.github.com.>
2026-07-29 14:34:48 +0530

    icon and username fix

--- Frontend/script.js/report.js
+++ Frontend/script.js/report.js
@@ -1,3 +1,18 @@
+const profile = document.querySelector(".profile");
+const profileBtn = document.querySelector(".profile-btn");
+const loggedUser = document.getElementById("loggedUser");
+
+if (loggedUser) {
+    loggedUser.textContent = localStorage.getItem("username");
+}
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+        localStorage.removeItem("username");
+    });
+}
 const weeklyData = {
 
     summary: {
