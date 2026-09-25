e16b939f89ea755fc2d17fdb53bbb651caf72c5b
SURAJ SINGH <250795727+surajsingh99245-ops@users.noreply.github.com>
2026-09-25 11:53:28 +0530

    Update login.js

--- Frontend/script.js/login.js
+++ Frontend/script.js/login.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://mindmate-qzot.onrender.com/";
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
