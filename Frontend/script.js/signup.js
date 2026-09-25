3d0fe95623cdfc1eef18a0e16221ab8c21b510f7
SURAJ SINGH <250795727+surajsingh99245-ops@users.noreply.github.com>
2026-09-25 11:52:20 +0530

    Update signup.js

--- Frontend/script.js/signup.js
+++ Frontend/script.js/signup.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://mindmate-qzot.onrender.com/";
 document.getElementById("signupForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
